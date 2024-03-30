934e9e1bb3e009cf01bab880b316c3ca12240828
Simon Nyberg <simon@nyberg.io>
2024-03-30 21:54:30 +0100

    markdown in treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		require'nvim-treesitter.configs'.setup {
 			-- A list of parser names, or "all" (the five listed parsers should always be installed)
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "rust", "swift" },
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "rust", "swift", "markdown" },
 
 			-- Install parsers synchronously (only applied to `ensure_installed`)
 			sync_install = false,
